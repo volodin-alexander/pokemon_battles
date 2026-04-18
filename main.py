48ef1e4f9f6017e722206ce8e5980509e87f5919
Sasha <s.volodin2014@gmail.com>
2026-04-18 20:59:52 +0600

    Added attacks lists and music

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 import pygame
 import random
+import easygui
 import tkinter as tk
 root = tk.Tk()
-canvas = tk.Canvas(root, width=400, height=200)
-canvas.pack()
-
-def animate_hp(target_hp, hp: int, damage: int):
-    global current_hp
-    if current_hp > target_hp:
-        hp -= damage  
-        
-        # New HP size
-        new_x1 = 50 + (current_hp * 2)
-        canvas.coords(hp_rect, 50, 50, new_x1, 80)
-        
-        # next step of animation after 10 milliseconds
-        root.after(10, lambda: animate_hp(target_hp, current_hp, 100))
+pygame.mixer.init()
+main_music = pygame.mixer.music.load('Trainer_Battle.mp3')
+pokemons = ['Pikachu', 'Vileplume', 'Charmonder', 'Squirtle', 'Iyisaur', 'Rattata']
+grass = ['Mega Drain', 'Leaf Hug', 'Growth', 'Vine Whip', 'Super Bud', 'Poison Powder', 'Poison Jab', 'Poison Sting', 'Acid']
+water = ['Water Gun', 'Surf', 'Bubble Beam', 'Hydro Pump', 'Scald', 'Sea Power', 'Wave', 'Hydro']
+elecric = ['Thunder Shock', 'Thunder', 'Thunderbolt', 'High Voltage', '220 Power', 'Thunder Punch']
+fire = ['Fire Blast', 'Flamethower', 'Fire Spin', 'Lava Soup', 'Earthflame', 'Fire Fly', 'Hyper Warm']
 
 
-hp_logic = 100    # HP
-current_hp = 100  # state in animation
-
-# rectangle
-canvas.create_rectangle(50, 50, 250, 80, fill="grey")
-hp_rect = canvas.create_rectangle(50, 50, 250, 80, fill="green")
 root.mainloop()
